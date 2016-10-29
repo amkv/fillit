10f01ca4faf972b2cb0af79b207c7ad229cf183e
Artem Kalmykov <kalmykov.artem@gmail.com>
2016-10-28 18:20:15 -0700

    ft_fillit.c

--- ft_fillit.c
+++ ft_fillit.c
@@ -23,22 +23,16 @@ int         *ft_fillit(int *grid, tetrimino **tetriminos, int size)
 	int 	yesno;
 
 	i = 0;
-	yesno = 1;
 	(size < 4) ? (gridsize = 16) : (gridsize = size * size);
 	while (size > 0)
 	{
-		if (ft_check_grid_space(grid, tetriminos[i]) == yesno)
-		{
-			printf("yes\n");
+		if (ft_check_grid_space(grid, tetriminos[i]) == 1)
 			grid = ft_place_tetro(grid, tetriminos[i]);
-		}
 		else
 		{
-			while (ft_check_grid_space(grid, tetriminos[i]) != yesno)
+			while (ft_check_grid_space(grid, tetriminos[i]) != 1)
 			{
-				printf("no\n");
 				ft_move_right(tetriminos[i], gridsize, 3);
-				yesno = -1;
 			}
 			grid = ft_place_tetro(grid, tetriminos[i]);
 		}
@@ -62,12 +56,6 @@ static int		*ft_place_tetro(int *grid, tetrimino *tetro)
 	grid[tetro->p2] = tetro->letter;
 	grid[tetro->p3] = tetro->letter;
 	grid[tetro->p4] = tetro->letter;
-//
-// 	grid[tetro->p1] = 'a';
-//	grid[tetro->p2] = 'b';
-//	grid[tetro->p3] = 'c';
-//	grid[tetro->p4] = 'd';
-
 	return (grid);
 }
 
@@ -80,22 +68,19 @@ static tetrimino   *ft_move_right(tetrimino *t, int gridsize, int right)
 	br = ft_grid_border(gridsize, right);
 	side = ft_sqrt(gridsize);
 	i = 0;
+
 //	while (i < side)
 //	{
-//		printf("%d ", br[i]);
-//		i++;
+//		if (t->p1 + 1 == br[i] || t->p2 + 1 == br[i] || t->p3 + 1 == br[i] || t->p4 + 1 == br[i])
+//
 //	}
-//	printf("\n");
 
-//	if (t->p1 + 1 != br[i] && t->p2 + 1 != br[i] && t->p3 + 1 != br[i] && t->p4 + 1 != br[i])
-//	{
-		t->p1 += 1;
-		t->p2 += 1;
-		t->p3 += 1;
-		t->p4 += 1;
-//	}
-//	else
-//		return (t);
+//	if (t->p1 + 1 == br[0])
+	t->p1 += 1;
+	t->p2 += 1;
+	t->p3 += 1;
+	t->p4 += 1;
+
 	free (br);
 	return (t);
 }
